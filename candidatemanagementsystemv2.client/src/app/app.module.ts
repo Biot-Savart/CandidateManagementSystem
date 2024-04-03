2f2dbf5323b40c884c2f75ef298fdd3516b4e6e6
Ryno Myburgh <ryno@babylonstoren.com>
2024-04-03 23:56:28 +0200

    organised imports

--- candidatemanagementsystemv2.client/src/app/app.module.ts
+++ candidatemanagementsystemv2.client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -7,24 +8,23 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { NgChartsModule } from 'ng2-charts';
 import { AuthGuard } from '../app/guards/auth.guard';
 import { AppComponent } from './app.component';
 import { CandidateFormComponent } from './components/candidate-form/candidate-form.component';
 import { CandidatesListComponent } from './components/candidates-list/candidates-list.component';
 import { DataReportComponent } from './components/data-report/data-report.component';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { PositionsDisplayPipe } from './pipes/positions-display.pipe';
 import { SkillsDisplayPipe } from './pipes/skills-display.pipe';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatMenuModule } from '@angular/material/menu';
-import { NgChartsModule } from 'ng2-charts';
 
 
 // Define routes
